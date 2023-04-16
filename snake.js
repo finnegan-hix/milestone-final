563a621fbf5c2e7f84d56563222bf26710cbc499
Finngodumb <finny12349@gmail.com>
2023-04-15 23:17:49 -0400

    second commit

--- snake.js
+++ snake.js
@@ -10,3 +10,38 @@ const snakeBorder = "black";
 const healthColor = " white";
 const size = 40;
 let running = false;
+let  horizontalMove = size;
+let verticalMove = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+    {x:size * 4, y:0},
+    {x:size * 3, y:0},
+    {x:size * 2, y:0},
+    {x:size, y:0},
+    {x:0, y:0}
+];
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
+gameStart();
+function gameStart(){
+    running= true;
+    scoreText.textContent = score;
+    createHealth();
+    drawFood();
+    resume();
+};
+function resume(){
+    if(running){ setTimeout(()=>{clear();
+            drawFood();
+            moveSnake();
+            drawSnake();
+            checkGameOver();
+            resume();
+        }, 75);
+    }
+    else{
+        displayGameOver();
+    }
+};
